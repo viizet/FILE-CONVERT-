abdf3b6d157f5eb6e50629f5f54e02f10b7757bd
VIIZET <129474307+viizet@users.noreply.github.com>
2023-12-26 18:15:05 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -4,18 +4,26 @@ from PIL import Image
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
-async def fix_thumb(thumb, max_width=320, max_height=320):
+async def fix_thumb(thumb):
+    width = 0
+    height = 0
     try:
         if thumb is not None:
-            img = Image.open(thumb)
-            img.thumbnail((max_width, max_height))  # Resize within the specified dimensions
-            img.save(thumb, "JPEG")
-            width, height = img.size
-            return width, height, thumb
+            metadata = extractMetadata(createParser(thumb))
+            if metadata.has("width"):
+                width = metadata.get("width")
+            if metadata.has("height"):
+                height = metadata.get("height")
+                Image.open(thumb).convert("RGB").save(thumb)
+                img = Image.open(thumb)
+                img.thumbnail((320, height))  # Resizing while maintaining aspect ratio
+                img.save(thumb, "JPEG")
     except Exception as e:
         print(e)
-        return None, None, None
-    
+        thumb = None 
+       
+    return width, height, thumb
+
 async def take_screen_shot(video_file, output_directory, ttl):
     out_put_file_name = f"{output_directory}/{time.time()}.jpg"
     file_genertor_command = [
@@ -34,17 +42,17 @@ async def take_screen_shot(video_file, output_directory, ttl):
         stderr=asyncio.subprocess.PIPE,
     )
     stdout, stderr = await process.communicate()
+    e_response = stderr.decode().strip()
+    t_response = stdout.decode().strip()
     if os.path.lexists(out_put_file_name):
         return out_put_file_name
     return None
 
-# Example usage
+# Example usage:
 thumbnail_path = "path/to/thumbnail.jpg"
 video_path = "path/to/video.mp4"
 output_directory = "path/to/output"
-max_width, max_height = 320, 320  # Desired maximum width and height
-
-thumbnail_width, thumbnail_height, resized_thumbnail = asyncio.run(fix_thumb(thumbnail_path, max_width, max_height))
+thumbnail_width, thumbnail_height, resized_thumbnail = asyncio.run(fix_thumb(thumbnail_path))
 
 if resized_thumbnail:
     screenshot_path = asyncio.run(take_screen_shot(video_path, output_directory, ttl=5))
