3c879e4d80f9b918d4bec087b9c0bec9a88a6fc1
VIIZET <129474307+viizet@users.noreply.github.com>
2023-12-26 18:26:25 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -1,3 +1,4 @@
+import time
 import os
 import asyncio
 from PIL import Image
@@ -8,7 +9,7 @@ async def fix_thumb(thumb):
     width = 0
     height = 0
     try:
-        if thumb is not None:
+        if thumb != None:
             metadata = extractMetadata(createParser(thumb))
             if metadata.has("width"):
                 width = metadata.get("width")
@@ -16,14 +17,14 @@ async def fix_thumb(thumb):
                 height = metadata.get("height")
                 Image.open(thumb).convert("RGB").save(thumb)
                 img = Image.open(thumb)
-                img.thumbnail((320, height))  # Resizing while maintaining aspect ratio
+                img.resize((320, height))
                 img.save(thumb, "JPEG")
     except Exception as e:
         print(e)
         thumb = None 
        
     return width, height, thumb
-
+    
 async def take_screen_shot(video_file, output_directory, ttl):
     out_put_file_name = f"{output_directory}/{time.time()}.jpg"
     file_genertor_command = [
@@ -47,20 +48,3 @@ async def take_screen_shot(video_file, output_directory, ttl):
     if os.path.lexists(out_put_file_name):
         return out_put_file_name
     return None
-
-# Example usage:
-thumbnail_path = "path/to/thumbnail.jpg"
-video_path = "path/to/video.mp4"
-output_directory = "path/to/output"
-thumbnail_width, thumbnail_height, resized_thumbnail = asyncio.run(fix_thumb(thumbnail_path))
-
-if resized_thumbnail:
-    screenshot_path = asyncio.run(take_screen_shot(video_path, output_directory, ttl=5))
-    if screenshot_path:
-        print(f"Thumbnail Size: {thumbnail_width}x{thumbnail_height}")
-        print(f"Resized Thumbnail Path: {resized_thumbnail}")
-        print(f"Screenshot Path: {screenshot_path}")
-    else:
-        print("Failed to capture screenshot.")
-else:
-    print("Failed to process thumbnail.")
